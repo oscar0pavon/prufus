7bfd2ffdc1c62e74b1708f41f542f624a11671c6
Oscar Pavon <pavon_oscar@outlook.com>
2025-10-12 19:07:44 +0000

    removed installer and usb_validator.c

--- source_code/installer/installer.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INSTALL_H_
-#define _INSTALL_H_
-
-#include <gtk/gtk.h>
-
-void install_prufus(GtkWidget *widget, gpointer data);
-
-#endif
--- source_code/installer/prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -vp /tmp/prufus
-#if already exist clean first
-rm -f /tmp/prufus/status
-
-echo 1 > /tmp/prufus/status
